bdb70e1a86e21539e8c51b261ab3a66b0b92aa0f
Ennis Massey <ennisbaradine@gmail.com>
2016-02-27 13:46:01 +1300

    Derp

--- AppBundler/AppBundler/AppDelegate.m
+++ AppBundler/AppBundler/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#include "MainWindow.h"
 
 @interface AppDelegate ()
 
--- AppBundler/AppBundler/ViewController.h
+++ AppBundler/AppBundler/ViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#include "MainWindow.h"
 
 @interface ViewController : NSViewController
 
