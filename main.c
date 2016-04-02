d23f8e39458389422b0d7190c42692999a58340e
Ennis Massey <ennisbaradine@gmail.com>
2016-04-02 14:22:37 +1300

    Initial Commit to C

---
+++ main.c
@@ -0,0 +1,9 @@
+//
+// Created by Ennis Massey on 25/03/16.
+//
+#include "main.h"
+
+int main() {
+    printf("Hello World");
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+//
+// Created by Ennis Massey on 25/03/16.
+//
+
+#ifndef BASE_INCLUDES
+#define BASIC_INCLUDES
+#include <stdio.h>
+#include <unistd.h>
+#include <fstream>
+
+#endif //BASIC_INCLUDES
